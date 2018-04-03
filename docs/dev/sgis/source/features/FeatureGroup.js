73515c755d7253742785eb6861fa9053fae6ea2a
Maxim Gritsenko <maxim@gritsenko.biz>
2018-04-03 17:37:25 +0000

    Travis build: 168

--- docs/dev/sgis/source/features/FeatureGroup.js
+++ docs/dev/sgis/source/features/FeatureGroup.js
@@ -11,26 +11,43 @@ define(["require", "exports", "./Feature", "../Point", "../symbols/point/Point",
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     class FeatureGroup extends Feature_1.Feature {
-        constructor(position, _a = {}) {
+        constructor(features, _a = {}) {
             var { symbol = new Point_2.PointSymbol() } = _a, params = __rest(_a, ["symbol"]);
             super(Object.assign({ symbol }, params));
-            this._position = position;
+            this._features = features;
         }
         projectTo(crs) {
             let projected = Point_1.Point.prototype.projectTo.call(this, crs);
             return new PointFeature_1.PointFeature(projected.position, { crs: crs, symbol: this.symbol });
         }
-        get features() {
+        centreOfMass() {
+            const [x, y] = this._features.reduce((prev, curr) => [prev[0] + curr.x, prev[1] + curr.y], [0, 0]);
+            return [x / this._features.length, y / this._features.length];
+        }
+        ;
+        features() {
             return this._features;
         }
-        get bbox() { return new Bbox_1.Bbox(this._position, this._position, this.crs); }
-        get position() { return [this._position[0], this._position[1]]; }
-        get x() { return this._position[0]; }
+        get position() {
+            const [x, y] = this.centreOfMass();
+            return [x, y];
+        }
+        set position(position) {
+            this._position = position;
+            this.redraw();
+        }
+        get bbox() {
+            const [x, y] = this.centreOfMass();
+            return new Bbox_1.Bbox([x, y], [x, y], this.crs);
+        }
+        get point() { return new Point_1.Point(this.position, this.crs); }
+        set point(point) { this.position = point.projectTo(this.crs).position; }
+        get x() { return this.centreOfMass()[0]; }
         set x(x) {
             this._position[0] = x;
             this.redraw();
         }
-        get y() { return this._position[1]; }
+        get y() { return this.centreOfMass()[1]; }
         set y(y) {
             this._position[1] = y;
             this.redraw();
