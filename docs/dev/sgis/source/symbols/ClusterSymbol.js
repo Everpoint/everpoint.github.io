792dfb8fb96187d951bbd3f353b0c129b61bbc0a
Maxim Gritsenko <maxim@gritsenko.biz>
2018-04-09 15:30:24 +0000

    Travis build: 180

--- docs/dev/sgis/source/symbols/ClusterSymbol.js
+++ docs/dev/sgis/source/symbols/ClusterSymbol.js
@@ -9,6 +9,10 @@ define(["require", "exports", "./Symbol", "../utils/utils", "../painters/DomPain
     align-items: center;
     -ms-flex-pack: center;
     justify-content: center;
+    border-radius: 50%;
+    box-sizing: border-box;
+    border-color: #fff;
+    border-style: solid;
 `;
     utils_1.setCssClasses({ [DEFAULT_WRAPPER_CLASS_NAME]: DEFAULT_WRAPPER_STYLE });
     const DEFAULT_SVG_CLASS_NAME = 'sGis-dynamicClusterSVG';
@@ -26,7 +30,7 @@ define(["require", "exports", "./Symbol", "../utils/utils", "../painters/DomPain
 `;
     utils_1.setCssClasses({ [DEFAULT_LABEL_CLASS_NAME]: DEFAULT_LABEL_STYLE });
     class ClusterSymbol extends Symbol_1.DynamicPointSymbol {
-        constructor({ node, count, wrapperClassNames = '', svgClassNames = '', labelClassNames = '', strokeWidth = 6, size = 44 - strokeWidth, values = [], offset = [-size / 2, -size / 2], colors = [], fill = '#fff', stroke = '#89CCF1', } = {}) {
+        constructor({ node, count, wrapperClassNames = '', svgClassNames = '', labelClassNames = '', borderWidth = 2, strokeWidth = 6, size = 44 - strokeWidth, values = [], offset = [-size / 2, -size / 2], colors = [], fill = '#fff', stroke = '#89CCF1', } = {}) {
             super({ offset });
             this.wrapperClassNames = wrapperClassNames
                 ? [DEFAULT_WRAPPER_CLASS_NAME].concat(wrapperClassNames.split(' '))
@@ -45,6 +49,7 @@ define(["require", "exports", "./Symbol", "../utils/utils", "../painters/DomPain
             this.stroke = stroke;
             this.node = node;
             this.count = count;
+            this.borderWidth = borderWidth;
         }
         polarToCartesian(centerX, centerY, radius, angleInDegrees) {
             const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
@@ -84,8 +89,8 @@ define(["require", "exports", "./Symbol", "../utils/utils", "../painters/DomPain
                 wrapper.appendChild(this.node);
             }
             else {
-                const radius = this.size / 2;
-                const r2 = this.size + this.strokeWidth;
+                const radius = this.size / 2 - this.borderWidth;
+                const r2 = this.size + this.strokeWidth - this.borderWidth;
                 const c = radius + this.strokeWidth / 2;
                 const svg = new SvgRender_1.SvgRender({
                     width: r2,
@@ -131,6 +136,7 @@ define(["require", "exports", "./Symbol", "../utils/utils", "../painters/DomPain
             }
             wrapper.style.width = `${this.size + this.strokeWidth}px`;
             wrapper.style.height = `${this.size + this.strokeWidth}px`;
+            wrapper.style.borderWidth = `${this.borderWidth}px`;
             wrapper.classList.add(...this.wrapperClassNames);
             return wrapper;
         }
